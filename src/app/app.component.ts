c6e1055a5d612229dd524defd61386919e3a6c1d
joscar03-dev <locomancocapac@gmail.com>
2024-08-23 20:32:46 -0500

    primero commit e inicio del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app_delivery';
+  //hacer una funcion que sea un contador
+  count = 0;
+  //hacer una funcion que sea un boton para incrementar el contador
+  incrementCount() {
+    this.count++;
+  }
 }
